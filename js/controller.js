82466410afba74218dc5689595b378367d966b82
Alex Oliveira <aoliveira@esec.com.br>
2015-11-27 20:10:00 -0200

    Adding modal to ask the password

--- js/controller.js
+++ js/controller.js
@@ -1,73 +1,30 @@
-"use strict";
+(function(global) {
+    var module = {};
 
-// random numbers generator
-var prng = sjcl.random;
+    /** Password to protected the passwords file. */
+    module.password = null;
 
-// characters for a randomly generated password
-var pwdSafeLetters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-var pwdUnsafeLetters = " !#$%&)*+,-./:;<=>?@]^_`|}";
-var pwdAllLetters = pwdSafeLetters + pwdUnsafeLetters;
+    module.passwordDeferred = null;
 
-// start adding entropy to PRNG
-window.onload = function() {
-	prng.startCollectors();
-}
+    var loadPassword = function() {
+        if (module.password) {
+            return Promise.resolve(module.password);
+        }
+        return $.Deferred(function(deferred) {
+            module.passwordDeferred = deferred;
+            $('#passwordModal').modal();
+        });
+    }
 
-function generate(safeOnly) {
-	safeOnly = safeOnly || false;
-	try {
-		var randomField = document.getElementById('randomField');
-		var nums = prng.randomWords(15);
-		var letters = (safeOnly ? pwdSafeLetters : pwdAllLetters);
-		var chars = '';
-		for (var i = 0; i < nums.length; i++) {
-			var pos = Math.abs(nums[i] % letters.length);
-			chars += letters.charAt(pos);
-		}
-		randomField.value = chars;
-	} catch(err) {
-		alert(err);
-	}
-}
+    // handle click on load button
+    $('#loadBtn').click(function(argument) {
+        loadPassword()
+        .then(gdrive.load)
+        .then(function(cipheredText) {
+            clearText = scrypto.decrypt(cipheredText, module.password);
+            $('#contentArea').val(clearText);
+        });
+    });
 
-// validate, consume and clear password fields
-function readPassword() {
-
-	// get fields and read values
-	var passwordField = document.getElementById('passwordField');
-	var password = passwordField.value;
-
-	// clear fields
-	passwordField.value = '';
-
-	// assert not blank
-	if (password === '') {
-		throw 'Password cannot be blank!';
-	}
-
-	return password;
-}
-
-// encrypt the content
-function encrypt() {
-	try {
-		var contentField = document.getElementById('contentField');
-		var password = readPassword();
-		var encrypted = sjcl.encrypt(password, contentField.value);
-		contentField.value = encrypted;
-	} catch(err) {
-		alert(err);
-	}
-}
-
-// decrypt the content
-function decrypt() {
-	try {
-	var contentField = document.getElementById('contentField');
-	var password = readPassword();
-	var decrypted = sjcl.decrypt(password, contentField.value);
-	contentField.value = decrypted;
-	} catch(err) {
-		alert(err);
-	}
-}
+    global.controller = module;
+})(this);
--- js/gdrive.js
+++ js/gdrive.js
@@ -1,35 +1,36 @@
 (function(global) {
-	var module = {};
+    var module = {};
 
-	var CLIENT_ID = '780199501376-q7dm26lauueljp43upe0jdt6jje5s255.apps.googleusercontent.com';
-	var SCOPES = ['https://www.googleapis.com/auth/drive'];
+    var CLIENT_ID = '780199501376-q7dm26lauueljp43upe0jdt6jje5s255.apps.googleusercontent.com';
+    var SCOPES = ['https://www.googleapis.com/auth/drive'];
 
-	module.load = function() {
-		return gapi.auth.authorize({client_id: CLIENT_ID, scope: SCOPES})
-		.then(function() {
-			return gapi.client.load('drive', 'v2');
-		}).then(function() {
-			var request = gapi.client.drive.files.list({
-				'q': "title = 'passwords.shadow'",
-				'fields': 'items(id,title)'
-			});
-			return new Promise(function(resolve, reject) {
-				request.execute(resolve);
-			});
-		}).then(function(resp) {
-			return new Promise(function(resolve, reject) {
-				var accessToken = gapi.auth.getToken().access_token;
-				var fileId = resp.items[0].id;
-				var xhr = new XMLHttpRequest();
-				xhr.open('GET', 'https://www.googleapis.com/drive/v2/files/'+fileId+'?alt=media');
-				xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-				xhr.onload = function() {
-					resolve(xhr.responseText);
-				};
-				xhr.send();
-			});
-		});
-	};
+    module.load = function() {
+        return gapi.auth.authorize({client_id: CLIENT_ID, scope: SCOPES})
+        .then(function() {
+            return gapi.client.load('drive', 'v2');
+        }).then(function() {
+            var request = gapi.client.drive.files.list({
+                q: "title = 'passwords.shadow'",
+                fields: 'items(id,title)',
+                immediate: true,
+            });
+            return new Promise(function(resolve, reject) {
+                request.execute(resolve);
+            });
+        }).then(function(resp) {
+            return new Promise(function(resolve, reject) {
+                var accessToken = gapi.auth.getToken().access_token;
+                var fileId = resp.items[0].id;
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', 'https://www.googleapis.com/drive/v2/files/'+fileId+'?alt=media');
+                xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+                xhr.onload = function() {
+                    resolve(xhr.responseText);
+                };
+                xhr.send();
+            });
+        });
+    };
 
-	global.gdrive = module;
+    global.gdrive = module;
 })(this);
