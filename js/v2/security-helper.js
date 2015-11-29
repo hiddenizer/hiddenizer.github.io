10d2348b08a44fea649fd296d789ad65749fd767
Alex Oliveira <alex.rodrigo.oliveira@gmail.com>
2015-11-29 18:21:55 -0200

    Removing v2 folder, moved to v2 branch

--- js/v2/security-helper.js
+++
@@ -1,9 +0,0 @@
-// encrypt the content
-function encrypt(data, password) {
-    return sjcl.encrypt(password, data);
-}
-
-// decrypt the content
-function decrypt(data, password) {
-    return sjcl.decrypt(password, data);
-}
