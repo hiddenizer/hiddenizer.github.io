a28c3bb8ea05c499ebb7d53933e209830ce5b020
Alex Oliveira <alex.rodrigo.oliveira@gmail.com>
2015-11-29 16:28:55 -0200

    Handling password modal events.
    Problems mixing promises and deferreds.

--- js/controller.js
+++ js/controller.js
@@ -4,16 +4,30 @@
     /** Password to protected the passwords file. */
     module.password = null;
 
-    module.passwordDeferred = null;
+    module.passwordDeferred = $.Deferred();
 
-    var loadPassword = function() {
+    /**
+     * Load the value of <code>module.password</code>.
+     *
+     * This function returns a promise of the value of the password to decrypt
+     * the file. The password is asked to the user using a modal.
+     *
+     * In addition to fullfilling the promise, the password is also set at
+     * <code>module.password</code>.
+     *
+     * @return {Promise} Promise of the password.
+     */
+    function loadPassword() {
         if (module.password) {
             return Promise.resolve(module.password);
         }
-        return $.Deferred(function(deferred) {
-            module.passwordDeferred = deferred;
-            $('#passwordModal').modal();
-        });
+        $('#passwordModal').modal();
+        return module.passwordDeferred.promise();
+    }
+
+    function enterPassword() {
+        module.password = $('#passwordField').val();
+        module.passwordDeferred.resolve(module.password);
     }
 
     // handle click on load button
@@ -24,6 +38,19 @@
             clearText = scrypto.decrypt(cipheredText, module.password);
             $('#contentArea').val(clearText);
         });
+    });
+
+    // put focus on password field when modal appears
+    $('#passwordModal').on('shown.bs.modal', function() {
+        $('#passwordField').focus();
+    });
+
+    $('#passwordEnterBtn').click(enterPassword);
+    $('#passwordField').keyup(function(event) {
+        if (event.keyCode === 13) {
+            $('#passwordModal').modal('hide');
+            enterPassword();
+        }
     });
 
     global.controller = module;
